ede50a032893c43dc29eda96a7291690821793a5
wilfredeveloper <pxumtech@gmail.com>
2024-06-21 01:43:41 +0300

    Implemented the onboarding page

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,46 @@
-import { Text, View } from "react-native";
+import { Redirect, router } from "expo-router";
+import { Image, ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
+import CustomButton from "../components/CustomButton";
+import { StatusBar } from "expo-status-bar";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="text-zinc-400 font-bold text-3xl">Homeseek</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full justify-center items-center px-4 min-h-[85vh]">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className="max-w-[300px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+
+          <View className="relative mt-5">
+            <Text className="font-bold text-3xl text-center text-white">
+              Discover endless possibilities with{" "}
+              <Text className="text-secondary">Aora</Text>
+            </Text>
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+              resizeMode="contain"
+            />
+          </View>
+          <Text className="text-sm text-gray-100 font-pregular mt-7 text-center">
+            Where creativity meets innovation: embark on a journey of limitless
+            exploration with Aora
+          </Text>
+          <CustomButton title="Continue with email" handlePress={() => {router.push('/sign-in')}} containerStyles="w-full mt-7"/>
+        </View>
+      </ScrollView>
+
+      <StatusBar backgroundColor="#161622" style="light"/>
+    </SafeAreaView>
   );
 }
