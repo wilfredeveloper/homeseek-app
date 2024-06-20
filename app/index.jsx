d0e31d7794aecae72bc04a03364637cc317fd476
wilfredeveloper <pxumtech@gmail.com>
2024-06-21 00:18:40 +0300

    Project Init: Setup tailwind

---
+++ app/index.jsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="text-zinc-400 font-bold text-3xl">Homeseek</Text>
+    </View>
+  );
+}
